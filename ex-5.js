6ae99241c237ec9a41b67464b997e90a036c186f
En <graphic.mimage@gmail.com>
2025-07-16 14:46:44 +0700

    Add solutions for assign2 exercises

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,20 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalprize ="";
+let discountamount="";
+let finalprize ="";
+
+totalprize = 27000;
+discountamount = 25;
+
+finalprize = totalprize - (totalprize * (discountamount/100))
+
+console.log("ราคาสินค้าทั้งหมด " , totalprize + " BATH");
+console.log("ส่วนลด % " , discountamount + " %");
+console.log("ราคาที่ต้องจ่าย " , finalprize + " BATH");
+
+
+
+
